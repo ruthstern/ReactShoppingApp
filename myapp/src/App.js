7aab98430594bdc94a7cfaf970ba6afa8e1bdd4f
Ruth Stern <68050491+ruthstern@users.noreply.github.com>
2022-05-12 09:29:35 +0200

    using react hooks

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,16 +3,31 @@ import Header from './Components/Header';
 import Main from './Components/Main';
 import Basket from './Components/basket';
 import Data from "./Data";
+import {useState} from "react";
 
 
 function App() {
   const { products } = Data;
+  const [cartItems, setCartItems] =useState ([]);
+  const onAdd = (product) =>{
+  const exist = cartItems.find (x=> x.id === product.id);
+  if (exist){
+    setCartItems (
+      cartItems.map ((x) =>
+      x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+      )
+    );
+      } else {
+        setCartItems ( [...cartItems, { ...product, qty:1 }]);
+      }
+    };
+    
   return (
     <div className="App">
     <Header></Header>
     <div className='row'>
-      <Main products= {products}></Main>
-      <Basket></Basket>
+      <Main onAdd= {onAdd} products= {products}></Main>
+      <Basket  onAdd ={onAdd} cartItems={cartItems}></Basket>
     </div>
     </div>
 
--- myapp/src/Components/Main.js
+++ myapp/src/Components/Main.js
@@ -2,13 +2,13 @@ import React from "react";
 import Product from "./Components/Products";
 
 export default function Main (props) {
-    const {products} = props;
+    const {products, onAdd} = props;
     return (
         <main className="block col-2">
             <h2> Products</h2> 
             <div className="row" >
                 { products.map((product) => (
-                    <product key={product.id} product={product}></product>
+                    <product key={product.id} product={product} onAdd = {onAdd}></product>
                 )) }
             </div>
         </main>
