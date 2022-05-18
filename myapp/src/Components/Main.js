1e1ba78c458daf731c8e91f5748ab34fd23e63dd
Ruth Stern <68050491+ruthstern@users.noreply.github.com>
2022-05-18 11:30:45 +0200

    Co-authored-by: Georgia Moldovan <georgimld@users.noreply.github.com>

--- myapp/src/Components/Main.js
+++ myapp/src/Components/Main.js
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 
-export default function Main(props) {
+/*export default function Main(props) {
     const { products, onAdd } = props;
     return (
         <Box sx={{ width: '100%', maxWidth: 800 }}>
@@ -16,8 +16,23 @@ export default function Main(props) {
                 {products.map((product) => (
                     <Product key={product.id} product={product} onAdd={onAdd} />
                 ))}
-            </div> */}
+            </div> }
             <Table/>
         </Box>
     );
 } 
+*/
+
+export default function Main(props) {
+  const { products, onAdd } = props;
+  return (
+    <main className="block col-2">
+      <h2>Products</h2>
+      <div className="row">
+        {products.map((product) => (
+          <Product key={product.id} product={product} onAdd={onAdd}></Product>
+        ))}
+      </div>
+    </main>
+  );
+}
