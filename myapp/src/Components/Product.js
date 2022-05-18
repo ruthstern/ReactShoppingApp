d639b4f0ddc388b6c43e6053dce010c0d3e2047d
Ruth Stern <68050491+ruthstern@users.noreply.github.com>
2022-05-18 11:31:34 +0200

    added buttons to the cart

--- myapp/src/Components/Product.js
+++ myapp/src/Components/Product.js
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 export default function Product(props) {
-  const {product, onAdd} = props;
+  const { product, onAdd } = props;
   return (
     <div>
-      <img className="small" src={product.image} alt={product.name}></img>
+      <img className="small" src={product.image} alt={product.name} />
       <h3>{product.name}</h3>
       <div>${product.price}</div>
       <div>
-      <button onClick={() => onAdd (product)}>Add to Cart</button>
+        <button onClick={() => onAdd(product)}>Add To Cart</button>
+      </div>
     </div>
-    </div>
-    
-  )
+  );
 }
